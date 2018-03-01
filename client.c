f2f72f226b202832b7d0d0ba36cde4d708ad6270
8bit <tburchfi@nd.edu>
2018-03-01 13:53:59 -0500

    Refactor client

--- client.c
+++ client.c
@@ -17,6 +17,50 @@
 const char *HOST = "localhost";
 const char *PORT = "9432";
 
+FILE *socket_dial(const char *host, const char *port);
+
+int main(int argc, char *argv[]) {
+
+    //Retreive size of client terminal in terms of rows and cols
+    //TODO: Poll in background for change in size to send updates
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+    /* Connect to remote machine */
+    FILE *client_file = socket_dial(HOST, PORT);
+    if (client_file == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    /* Read from stdin and send to server */
+
+    //Make new request and check and stuff
+    request *req = malloc(sizeof(request));
+    response *res = malloc(sizeof(response));
+    if(!(req && res)){
+        perror("REQ_CREATE");
+    }
+
+    //Send connection
+    req->type = REQCONNECT;
+
+    //Grab terminal size from client user
+    req->content.connect.width = w.ws_col;
+    req->content.connect.height = w.ws_row;
+
+    if (fwrite(req, sizeof(request), 1, client_file) < 0) {
+      printf("error!\n");
+    }
+    fread(res, sizeof(response), 1, client_file);
+    if (res->type == RESCONNECT) {
+      printf("My id is: %i\n", res->content.connect.session_id);
+    } else {
+      printf("error\n");
+    }
+    fclose(client_file);
+    return EXIT_SUCCESS;
+}
+
 FILE *socket_dial(const char *host, const char *port) {
     /* Lookup server address information */
     struct addrinfo *results;
@@ -64,48 +108,6 @@ FILE *socket_dial(const char *host, const char *port) {
     }
 
     return client_file;
-}
-
-int main(int argc, char *argv[]) {
-
-    //Retreive size of client terminal in terms of rows and cols
-    //TODO: Poll in background for change in size to send updates
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
-    /* Connect to remote machine */
-    FILE *client_file = socket_dial(HOST, PORT);
-    if (client_file == NULL) {
-        return EXIT_FAILURE;
-    }
-
-    /* Read from stdin and send to server */
-
-    //Make new request and check and stuff
-    request *req = malloc(sizeof(request));
-    response *res = malloc(sizeof(response));
-    if(!(req && res)){
-        perror("REQ_CREATE");
-    }
-
-    //Send connection
-    req->type = REQCONNECT;
-
-    //Grab terminal size from client user
-    req->content.connect.width = w.ws_col;
-    req->content.connect.height = w.ws_row;
-
-    if (fwrite(req, sizeof(request), 1, client_file) < 0) {
-      printf("error!\n");
-    }
-    fread(res, sizeof(response), 1, client_file);
-    if (res->type == RESCONNECT) {
-      printf("My id is: %i\n", res->content.connect.session_id);
-    } else {
-      printf("error\n");
-    }
-    fclose(client_file);
-    return EXIT_SUCCESS;
 }
 
 /* vim: set expandtab sts=4 sw=4 ts=8 ft=c: */
